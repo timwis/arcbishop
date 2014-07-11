6ce0027a566f0734ffc83873fe8bb62ff9276677
Tim Wisniewski <tim@timwis.com>
2014-07-11 02:18:23 +0000

    Working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,5 +4,11 @@
         attribution = 'City of Philadelphia',
         layer = L.tileLayer(url, {maxZoom: 17, attribution: attribution, subdomains: 'abcd'});
     
-    map.setView([39.9524, -75.1636], 11).addLayer(layer);
+    map.setView([39.9524, -75.1636], 12).addLayer(layer);
+    
+    $(document).ready(function() {
+        $('[data-toggle]').on('click', function(e) {
+            $('body').removeClass('viewing-map').removeClass('viewing-list').addClass('viewing-' + $(this).data('toggle'));
+        });
+    });
 })(window.jQuery);
