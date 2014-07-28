dcf1584b352f53e1507456ba3c74a9d729e1e5fe
Patrick Hammons <patrick.t.hammons@gmail.com>
2014-07-28 08:58:16 -0400

    Add retina/hd display detection
    So the map doesn't look bad on mobile and retina

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
     var map = new L.Map('map'),
         url = 'http://gis.phila.gov/arcgis/rest/services/BaseMaps/GrayBase_WM/MapServer/tile/{z}/{y}/{x}',
         attribution = 'City of Philadelphia',
-        layer = L.tileLayer(url, {maxZoom: 17, attribution: attribution, subdomains: 'abcd'});
+        layer = L.tileLayer(url, {maxZoom: 17, attribution: attribution, subdomains: 'abcd', detectRetina: true});
     
     // Zoom to Philly, add basemap
     map.setView([39.9524, -75.1636], 12).addLayer(layer);
